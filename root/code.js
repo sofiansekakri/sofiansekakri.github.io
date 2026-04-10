a04c21bbb112062dae54d2464b385471e5b9a0b6
Sofian S. <143905862+sofiansekakri@users.noreply.github.com>
2026-04-10 21:05:34 +0200

    Add click event to open a URL in a new tab

--- root/code.js
+++ root/code.js
@@ -2,4 +2,7 @@
   'use strict';
   if (!$ || !$.ajax) return;
   console.log('ready', window.location.href);
+  $('.urlredirect').on('click', () => {
+    window.open('https://community.fandom.com/wiki/User:Crazybloy2');
+  });
 })();
